0aa4dd9bb002c8fa91e130c98a7072afd3e873e8
rjwyse <wyse.rob27@gmail.com>
2023-10-20 13:39:57 -0400

    MERN-Stack Infrastructure

---
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -0,0 +1,13 @@
+export default function OrderHistoryPage () {
+
+   async function handleCheckToken() {
+      alert('clicked');
+   }
+
+   return(
+      <>
+    <h1>OrderHistoryPage</h1>
+    <button onClick={ handleCheckToken } >Check When My Login Expires</button>
+    </>
+   ); 
+}
